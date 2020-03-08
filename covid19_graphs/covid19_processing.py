8b68665dbfdf6e5ffc70fd1d5ffd2bb0f5270f95
DLBPointon <damonlbp@hotmail.co.uk>
2020-03-08 22:55:22 +0000

    Small update to correct pycodestyle and pylint errors

--- covid19_graphs/covid19_processing.py
+++ covid19_graphs/covid19_processing.py
@@ -1,22 +1,11 @@
 """module for Covid19Processing class"""
 import logging
-import requests
-import sys
-import os
-import csv
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from datetime import datetime
-import string
 import math
-
-file_names = ['time_series_19-covid-Confirmed.csv',
-              'time_series_19-covid-Deaths.csv',
-              'time_series_19-covid-Recovered.csv']
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/'
-folder = 'csse_covid_19_data/csse_covid_19_time_series/'
-full_url = f'{url}{folder}'
+import os
+import sys
+import requests
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class Covid19Processing:
@@ -29,16 +18,15 @@ class Covid19Processing:
         self.out_dir = out_dir
         self.full_url = full_url
         print(filename)
-        logging.debug('Covid19Processing __init__ to be written')  # TODO
-        # TODO add instance variables to store the data here
-        # TODO decide on how we are going to store
+        logging.debug('Covid19Processing __init__ to be written')
 
     def create_out_dir(self):
         """creates a new output directory out_dir
         This will be used for all files to be written"""
 
         logging.debug(f'create_out_dir called'
-                      f'Output directory to be created: {self.out_dir}')
+                      f'Output directory to be created:'
+                      f' {self.out_dir}')
 
         access_rights = 0o755
         list_outs = ['downloaded', 'edited_csv', 'graphics']
@@ -50,11 +38,11 @@ class Covid19Processing:
                 try:
                     os.makedirs(path, access_rights)
                 except OSError:
-                    logging.debug(f'Creation of directory has failed at:'
-                                  f'{path}')
+                    logging.debug(f'Creation of directory has failed '
+                                  f'at: {path}')
                 else:
-                    logging.debug(f'''Successfully created the directory path at:
-                                        {path}''')
+                    logging.debug(f'Successfully created the '
+                                  f'directory path at:{path}')
         return self.out_dir, list_outs
 
     def download_from_github(self):
@@ -71,7 +59,8 @@ class Covid19Processing:
                       'wb') as csv_written:
                 csv_written.write(self.response.content)
         else:
-            print(f'Error in requests download status_code={status_code}')
+            print(f'Error in requests download'
+                  f'status_code={status_code}')
             sys.exit()
 
         return self.response
@@ -80,11 +69,13 @@ class Covid19Processing:
         """processes the stored data into a form for CSV files"""
         logging.debug('process_data called')
 
-        pd_time_series = pd.read_csv(f'{self.out_dir}/downloaded/{self.filename}')
+        pd_time_series = pd.read_csv(f'{self.out_dir}/downloaded/'
+                                     f'{self.filename}')
         pd_time_series = pd_time_series.drop('Lat', axis=1)
         pd_time_series = pd_time_series.drop('Long', axis=1)
         no_of_dates = len(pd_time_series.columns) - 2
-        dateindex = pd.date_range(start='1-22-2020', periods=no_of_dates, freq='D').strftime('%d-%m')
+        dateindex = pd.date_range(start='1-22-2020', periods=no_of_dates,
+                                  freq='D').strftime('%d-%m')
 
         new_cols = ['Province/State', 'Country/Region']
         for index in dateindex:
@@ -96,7 +87,10 @@ class Covid19Processing:
         # pd_edit_series = pd_time_series
 
         pd_edit_series = pd_edit_series.T
-        # pd_edit_series.loc[:, 'Daily Total'] = pd_edit_series.sum(numeric_only=True, axis=1)
+
+        # Future addition
+        # pd_edit_series.loc[:, 'Daily Total'] =
+        # pd_edit_series.sum(numeric_only=True, axis=1)
 
         return pd_edit_series
 
@@ -104,21 +98,27 @@ class Covid19Processing:
         """writes CSV files to out_dir"""
         logging.debug(f'write_csv_files called. File saved to:'
                       f'{self.out_dir}/edited_csv/{self.filename}')
-        pd_edit_series.to_csv(f'{self.out_dir}/edited_csv/edited_{self.filename}', encoding='utf-8')
+
+        pd_edit_series.to_csv(f'{self.out_dir}/edited_csv/'
+                              f'edited_{self.filename}',
+                              encoding='utf-8')
 
     def data(self, pd_edit_series):
         """
-        A function where the imported data will be further edited in a more extensive manner.
+        A function where the imported data will be further
+        edited in a more extensive manner.
         """
         europe = ['France', 'Spain', 'Italy', 'Belgium', 'Finland',
-                  'Sweden', 'Germany', 'Croatia', 'Switzerland', 'Austria',
-                  'Greece', 'Hungary', 'Slovenia', 'Gibraltar', 'Poland',
-                  'Bosnia and Herzegovina', 'Faroe Islands', 'Liechtenstein',
-                  'Ukraine', 'North Macedonia', 'Latvia', 'Andorra', 'Norway',
-                  'Portugal', 'Romania', 'Denmark', 'Estonia', 'Netherlands',
-                  'San Marino', 'Belarus', 'Iceland', 'Lithuania', 'Ireland',
-                  'Luxembourg', 'Monaco', 'Czech Republic', 'Vatican City',
-                  'Slovakia', 'Serbia', 'Malta']
+                  'Sweden', 'Germany', 'Croatia', 'Switzerland',
+                  'Austria', 'Greece', 'Hungary', 'Slovenia',
+                  'Gibraltar', 'Poland', 'Bosnia and Herzegovina',
+                  'Faroe Islands', 'Liechtenstein', 'Ukraine',
+                  'North Macedonia', 'Latvia', 'Andorra', 'Norway',
+                  'Portugal', 'Romania', 'Denmark', 'Estonia',
+                  'Netherlands', 'San Marino', 'Belarus', 'Iceland',
+                  'Lithuania', 'Ireland', 'Luxembourg', 'Monaco',
+                  'Czech Republic', 'Vatican City', 'Slovakia',
+                  'Serbia', 'Malta']
 
         asia = ['Thailand', 'Japan',
                 'South Korea', 'Taiwan', 'Macau',
@@ -150,28 +150,39 @@ class Covid19Processing:
         main_china_csv = pd_edit_series.loc[:, 'Mainland China'].copy()
         uk_csv = pd_edit_series.loc[:, 'UK'].copy()
         diamond_csv = pd_edit_series.loc[:, 'Others'].copy()
-        csv_list = {'europe': europe_csv, 'america': americas_csv, 'asia': asia_csv,
-                    'main_china': main_china_csv, 'UK': uk_csv, 'diamond': diamond_csv}
+        csv_list = {'europe': europe_csv, 'america': americas_csv,
+                    'asia': asia_csv, 'main_china': main_china_csv,
+                    'UK': uk_csv, 'diamond': diamond_csv}
 
-        pd_edit_series['Mainland_China_Total'] = pd_edit_series.loc[:, 'Mainland China'].sum(axis=1)
+        pd_edit_series['Mainland_China_Total'] = \
+            pd_edit_series.loc[:, 'Mainland China'].sum(axis=1)
 
-        pd_edit_series['US_Total'] = pd_edit_series.loc[:, 'US'].sum(axis=1)
+        pd_edit_series['US_Total'] = \
+            pd_edit_series.loc[:, 'US'].sum(axis=1)
 
-        pd_edit_series['Canada_Total'] = pd_edit_series.loc[:, 'Canada'].sum(axis=1)
+        pd_edit_series['Canada_Total'] = \
+            pd_edit_series.loc[:, 'Canada'].sum(axis=1)
 
-        pd_edit_series['Australia_Total'] = pd_edit_series[['Australia', 'New Zealand']].sum(axis=1)
+        pd_edit_series['Australia_Total'] = \
+            pd_edit_series[['Australia', 'New Zealand']].sum(axis=1)
 
-        pd_edit_series['Europe_Total'] = pd_edit_series[europe + ['UK']].sum(axis=1)
+        pd_edit_series['Europe_Total'] = \
+            pd_edit_series[europe + ['UK']].sum(axis=1)
 
-        pd_edit_series['Diamond_Princess'] = pd_edit_series[['Others']]
+        pd_edit_series['Diamond_Princess'] = \
+            pd_edit_series[['Others']]
 
-        pd_edit_series['UK_Total'] = pd_edit_series[['UK']]
+        pd_edit_series['UK_Total'] = \
+            pd_edit_series[['UK']]
 
-        pd_edit_series['Asian_Total'] = pd_edit_series[asia].sum(axis=1)
+        pd_edit_series['Asian_Total'] = \
+            pd_edit_series[asia].sum(axis=1)
 
-        pd_edit_series['Americas_Total'] = pd_edit_series[americas].sum(axis=1)
+        pd_edit_series['Americas_Total'] = \
+            pd_edit_series[americas].sum(axis=1)
 
-        pd_edit_series['African_Total'] = pd_edit_series[africa].sum(axis=1)
+        pd_edit_series['African_Total'] = \
+            pd_edit_series[africa].sum(axis=1)
 
         pd_edit_series = pd_edit_series.drop('Mainland China', axis=1)
         pd_edit_series = pd_edit_series.drop('US', axis=1)
@@ -198,14 +209,21 @@ class Covid19Processing:
         """
         logging.debug(f'write_new_csvs called. File saved to:'
                       f'{self.out_dir}edited_csv/')
-        pd_edit_series.to_csv(f'{self.out_dir}edited_csv/edited_location_totals', encoding='utf-8')
+
+        pd_edit_series.to_csv(f'{self.out_dir}edited_csv/'
+                              f'edited_location_totals',
+                              encoding='utf-8')
 
         for country, csv in csv_list.items():
             csv.to_csv(f'{self.out_dir}edited_csv/edited_{country}')
 
-    def round_up(self, n, decimals=0):
+    def round_up(self, number, decimals=0):
+        """
+        A function to aid in the production of more
+        flexible y axis integers
+        """
         multiplier = 10 ** decimals
-        return math.ceil(n * multiplier) / multiplier
+        return math.ceil(number * multiplier) / multiplier
 
     def plot_data(self, data):
         """
@@ -219,39 +237,52 @@ class Covid19Processing:
         data = data.drop('Canada_Total', axis=1)
 
         for column in data.columns:
-            data['Rest of the World'] = data.loc[:, data.columns != str(column)].sum(axis=1)
-            x = data.index.values
-            fig, ax = plt.subplots()
-            ax.plot(x, data[column], marker='o', label=column)
-            ax.plot(x, data['Rest of the World'], marker='s', label='Rest of the World')
+            subset = data.loc[:, data.columns != str(column)]
+
+            data['Rest of the World'] = subset.sum(axis=1)
+            x_axis = data.index.values
+
+            fig, axes = plt.subplots()
+            axes.plot(x_axis, data[column], marker='o',
+                      label=column)
+            axes.plot(x_axis, data['Rest of the World'], marker='s',
+                      label='Rest of the World')
 
             every_nth = 4
-            for number, label in enumerate(ax.xaxis.get_ticklabels()):
+            for number, label in enumerate(axes.xaxis.get_ticklabels()):
                 if number % every_nth != 0:
                     label.set_visible(False)
-            ax.set(xlabel='Date', ylabel='Cases',
-                   title=f'Covid-19 {final_title[0]} cases for {column} - data from John Hopkins CSSE')
-            ax.grid()
-            ax.legend()
+            axes.set(xlabel='Date', ylabel='Cases',
+                     title=f'Covid-19 {final_title[0]} cases for '
+                           f'{column} - data from John Hopkins CSSE')
+            axes.grid()
+            axes.legend()
 
             # Setting the y axis
             data_max = data.max(axis=1)
             max_number = data_max[-1]
             rounded_max = self.round_up(max_number, -3)
             rounded_max += 2000
-            ax.set_ylim([0, rounded_max])
+            axes.set_ylim([0, rounded_max])
 
-            y1 = data[column][-1]
-            y2 = data['Rest of the World'][-1]
+            y_axis1 = data[column][-1]
+            y_axis2 = data['Rest of the World'][-1]
 
             # Adds Labels to annotate the last data point for each plot
-            plt.annotate(y1, (x[-1], y1+500), bbox=dict(facecolor='blue', alpha=0.5), fontsize = 12)
-            plt.annotate(y2, (x[-1], y2+500), bbox=dict(facecolor='red', alpha=0.5), fontsize = 12)
+            plt.annotate(y_axis1, (x_axis[-1], y_axis1+500),
+                         bbox=dict(facecolor='blue', alpha=0.5),
+                         fontsize=12)
+            plt.annotate(y_axis2, (x_axis[-1], y_axis2+500),
+                         bbox=dict(facecolor='red', alpha=0.5),
+                         fontsize=12)
 
-            # Required in order to stop the column from summing the total of each run through the loop
+            # Required in order to stop the column from summing
+            # the total of each run through the loop
             data = data.drop('Rest of the World', axis=1)
-            dir_name = f'{self.out_dir}graphics/{final_title[0]}_for_{column}.png'
-            fig.savefig(dir_name, transparent=False, dpi=300, bbox_inches="tight")
+            dir_name = f'{self.out_dir}graphics/' \
+                       f'{final_title[0]}_for_{column}.png'
+            fig.savefig(dir_name, transparent=False, dpi=300,
+                        bbox_inches="tight")
             if os.path.exists(dir_name):
                 print(f'File saved at:'
                       f'{dir_name}')
